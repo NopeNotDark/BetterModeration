7e74d862f42ba2ba1f517ae69966a9a35b60e806
Colby Pham <61342696+NopeNotDark@users.noreply.github.com>
2023-06-13 22:18:30 -0400

    Fix mistake in sqlite.sql

--- resources/database/sqlite.sql
+++ resources/database/sqlite.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS punishments (id INTEGER PRIMARY KEY AUTO_INCREMENT, target TEXT NOT NULL, reason TEXT NOT NULL, staff TEXT NOT NULL, duration TEXT NOT NULL, timeAt INT NOT NULL);
 -- # }
 -- #  { add
-INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (modType, target, reason, staff, duration, timeAt);
+INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (:modType, :target, :reason, :staff, :duration, :timeAt);
 -- # }
 -- #  { remove
 UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modType = :modType
