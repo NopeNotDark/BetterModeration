7ba879aa823c222dea3d6d19f23a46678d6eca64
Colby Pham <61342696+NopeNotDark@users.noreply.github.com>
2023-06-13 21:27:12 -0400

    Update mysql.sql

--- resources/database/mysql.sql
+++ resources/database/mysql.sql
@@ -1,22 +1,16 @@
+-- #! mysql
 -- #{ bettermoderation
--- { init
-CREATE TABLE IF NOT EXISTS punishments (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    modType INTEGER NOT NULL,
-    target TEXT NOT NULL,
-    reason TEXT NOT NULL,
-    staff TEXT NOT NULL,
-    duration TEXT NOT NULL,
-    timeAt INTEGER NOT NULL
-);
--- }
--- { add
-INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (:modType, :target, :reason, :staff, :duration, :timeAt);
--- }
--- { remove
-UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modType = :modType;
--- }
--- { getAll
-SELECT * FROM punishments WHERE target = :target AND modType = :modType;
--- }
+-- # { init
+CREATE TABLE IF NOT EXISTS punishments (id INTEGER PRIMARY KEY AUTOINCREMENT, target TEXT NOT NULL, reason TEXT NOT NULL, staff TEXT NOT NULL, duration TEXT NOT NULL, timeAt INT NOT NULL);
+-- # }
+-- #  { add
+INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (modType, target, reason, staff, duration, timeAt);
+-- # }
+-- #  { remove
+UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modType = :modType
+-- # }
+-- # { getAll
+SELECT * FROM punishments WHERE target = :target AND modType = :modType
+-- # }
+
 -- #}
