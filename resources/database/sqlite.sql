647f8076d56d04629e8a37fc9cefdfe2feda4b82
Colby Pham <61342696+NopeNotDark@users.noreply.github.com>
2023-06-13 21:26:48 -0400

    Update sqlite.sql

--- resources/database/sqlite.sql
+++ resources/database/sqlite.sql
@@ -1,24 +1,15 @@
 -- #! sqlite
 -- #{ bettermoderation
-        -- { init
-CREATE TABLE IF NOT EXISTS punishments (
-  id INT NOT NULL AUTOINCREMENT,
-  modType INT NOT NULL,
-  target TEXT NOT NULL,
-  reason TEXT NOT NULL,
-  staff TEXT NOT NULL,
-  duration TEXT NOT NULL,
-  timeAt INT NOT NULL,
-  PRIMARY KEY (id)
-);
-        -- }
-        -- { add
+-- # { init
+CREATE TABLE IF NOT EXISTS punishments (id INTEGER PRIMARY KEY AUTOINCREMENT, target TEXT NOT NULL, reason TEXT NOT NULL, staff TEXT NOT NULL, duration TEXT NOT NULL, timeAt INT NOT NULL);
+-- # }
+-- #  { add
 INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (modType, target, reason, staff, duration, timeAt);
-        -- }
-        -- { remove
+-- # }
+-- #  { remove
 UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modType = :modType
-        -- }
-        -- { getAll
+-- # }
+-- # { getAll
 SELECT * FROM punishments WHERE target = :target AND modType = :modType
-        -- }
+-- # }
 -- #}
