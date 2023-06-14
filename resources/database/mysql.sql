6df84965836f524d32bb561b7b09b0ea2ea36596
Colby Pham <61342696+NopeNotDark@users.noreply.github.com>
2023-06-13 22:19:15 -0400

    Fix mistakes in mysql.sql

--- resources/database/mysql.sql
+++ resources/database/mysql.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS punishments (id INTEGER PRIMARY KEY AUTOINCREMENT, target TEXT NOT NULL, reason TEXT NOT NULL, staff TEXT NOT NULL, duration TEXT NOT NULL, timeAt INT NOT NULL);
 -- # }
 -- #  { add
-INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (modType, target, reason, staff, duration, timeAt);
+INSERT INTO punishments (modType, target, reason, staff, duration, timeAt) VALUES (:modType, :target, :reason, :staff, :duration, :timeAt);
 -- # }
 -- #  { remove
 UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modType = :modType
@@ -12,5 +12,4 @@ UPDATE punishments SET duration = duration - 20 WHERE target = :target AND modTy
 -- # { getAll
 SELECT * FROM punishments WHERE target = :target AND modType = :modType
 -- # }
-
 -- #}
